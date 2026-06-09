8a597f34a99371adb5c34d3e947bf1f717888317
Tsimofey Terashkevich <t861727861727@gmail.com>
2026-06-09 19:33:42 +0300

    Do test07

---
+++ test07project/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void init_random(int* array, int size) {
+	for (int i = 0; i < size; i++) {
+		*(array + i) = rand() % 100;
+	}
+}
+
+int main() {
+
+	int size;
+
+	cout << "Input size of array: ";
+	cin >> size;
+
+	int* array = new int[size] {};
+
+	init_random(array, size);
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+
+	delete[] array;
+
+	return 0;
+}
